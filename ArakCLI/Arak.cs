924d8b5d0137cf6974fd753007547828b2268056
Csiger Imre Krisztián <csigerik@KKSZKI.LOCAL>
2026-03-30 09:55:47 +0200

    2. és 3. feladat teljesítve

---
+++ ArakCLI/Arak.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArakCLI
+{
+    public class Arak
+    {
+        public Arak(int december, int januar, string kod, string megnevezes)
+        {
+            December = december;
+            Januar = januar;
+            Kod = kod;
+            Megnevezes = megnevezes;
+        }
+
+        public Arak(string sor) 
+        {
+            string[] temp = sor.Split('\t');
+            Kod = temp[0];
+            Megnevezes = temp[1].Split(",")[0];
+            Januar = int.Parse(temp[2]);
+            December = int.Parse(temp[3]);
+        }
+
+        public int December { get;  set; }
+        public int Januar { get;  set; }
+        public string Kod { get;  set; }
+        public string Megnevezes { get;  set; }
+
+
+    }
+}
--- ArakCLI/Program.cs
+++ ArakCLI/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace ArakCLI
+﻿
+
+
+namespace ArakCLI
 {
-    internal class Program
+    public class Program
     {
+        public static List<Arak> arakCollection = new List<Arak>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Beolvasas();
+            Feladat2();
+            Feladat3();
+        }
+
+        private static void Feladat3()
+        {
+            Console.WriteLine("3. feladat:");
+            Console.Write("Kérem adja meg a termék kódját! ");
+            string kod = Console.ReadLine();
+            var arAtlaga = arakCollection.FirstOrDefault(a => a.Kod == kod);
+            if (arAtlaga != null)
+            {
+                Console.WriteLine($"Termék neve: {arAtlaga.Megnevezes}, átlagára 2024-ben: {(arAtlaga.December + arAtlaga.Januar) / 2:F1} Ft");
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen termék a listában :(");
+            }
+        }
+
+        private static void Feladat2()
+        {
+            Console.WriteLine("1. feladat:");
+            Console.WriteLine($"Az állományban {arakCollection.Count} db termék található.");
+        }
+
+        private static void Beolvasas()
+        {
+            StreamReader streamReader = new StreamReader("Arak2024.txt");
+            streamReader.ReadLine();
+            while (!streamReader.EndOfStream)
+            {
+                Arak arak = new Arak(streamReader.ReadLine());
+                arakCollection.Add(arak);
+            }
+            streamReader.Close();
         }
     }
 }
