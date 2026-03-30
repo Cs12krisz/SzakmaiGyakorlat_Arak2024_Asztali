ee1ed4ca4b4fe9516f2b3ff694ff6e35cb04b8b8
Csiger Imre Krisztián <csigerik@KKSZKI.LOCAL>
2026-03-30 10:23:26 +0200

    Tesztek teljesítése, WPF hozzáadása

---
+++ ArakCLITests/ArakTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArakCLI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArakCLI.Tests
+{
+    [TestClass()]
+    public class ArakTests
+    {
+        [DataRow(5000, 10000, -5000)]
+        [DataRow(10000, 5000, 5000)]
+        [DataRow(5000, 5000, 0)]
+        [TestMethod()]
+        public void ValtozasTest(int december, int januar, int elvart)
+        {
+            Arak arak = new Arak(december, januar, "00000", "Termék");
+            Assert.AreEqual(elvart, arak.Valtozas());
+        }
+    }
+}
